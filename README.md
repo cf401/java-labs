6863c3bbf15aa2b25af486efd3e8bab1c2ce3f40
Jack Daniel Kinne <bravelemming@gmail.com>
2019-08-07 10:42:21 -0700

    tests written

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # java fundamentals 
 
 this file holds the first ten java labs for code fellows 401.
+
---
+++ basics/src/main/java/basics/Library.java
@@ -0,0 +1,26 @@
+//Author: Jack Daniel Kinne
+
+package basics;
+
+public class Library {
+    public int[] roll(int n) {
+        int[] diceValues = new int[n];
+        return diceValues;
+    }
+
+    public boolean containsDuplicates(int[] arr){
+        return false;
+    }
+
+    public int calculateAverage(int[] arr){
+        int averageValues = 0;
+        return averageValues;
+    }
+
+    public int[] arrayArrayLowAverage(int[][]arr){
+        //TODO: choose length of array for return when we know how long it should be
+        int[] averageLowestValues = new int[1];
+        return averageLowestValues;
+
+    }
+}
